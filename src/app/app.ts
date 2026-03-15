04454b8968073e627ff8fc9c4b5a4581e68ab402
Berkin Sansal <berkinsansal@gmail.com>
2026-03-16 01:56:35 +0300

    chore: replace default Angular landing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('buried-proof-game');
+  protected readonly title = signal('Buried Proof');
 }
