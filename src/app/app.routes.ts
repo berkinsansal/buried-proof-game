7b1ddcd2436f10f2d70e1aae38718c4d9a22ea7b
Berkin Sansal <berkinsansal@gmail.com>
2026-03-16 02:26:29 +0300

    feat: add intro page and initial routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Intro } from './features/intro/intro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Intro
+  }
+];
